00d2d1969e6fab3085ed31dffebf7ad7ce421427
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-03 22:36:43 +0200

    deleted basic settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,13 @@
 import './App.css'
-import styled from "styled-components";
 
-function App() {
+
+export const App = () => {
     return (
         <div className="App">
-            
+
         </div>
     )
 }
 
-export default App
 
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
-`;
+
