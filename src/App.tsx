a0fe9d26d0476eca4a2e9dcbd24dda877f6b8b1f
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-03 23:59:12 +0200

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import {Header} from "./layout/header/Header.tsx";
 
 
 export const App = () => {
     return (
         <div className="App">
-
+            <Header />
         </div>
     )
 }
---
+++ src/layout/header/Header.tsx
@@ -0,0 +1,29 @@
+
+
+export const Header = () => {
+    return (
+        <header>
+            <a href="">
+                <img src="" alt="logo"/>
+            </a>
+            <ul>
+                <li>
+                    <a href="#"></a>
+                </li>
+                <li>
+                    <a href="#"></a>
+                </li>
+                <li>
+                    <a href="#"></a>
+                </li>
+                <li>
+                    <a href="#"></a>
+                </li>
+                <li>
+                    <a href="#"></a>
+                </li>
+            </ul>
+        </header>
+    );
+};
+
