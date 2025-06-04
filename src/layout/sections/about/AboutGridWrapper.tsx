412cd68971f94a0d5b8913afca551f1805120ec0
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-04 15:05:18 +0200

    created AboutGridWrapper

---
+++ src/layout/sections/about/AboutGridWrapper.tsx
@@ -0,0 +1,68 @@
+import styled from "styled-components";
+
+type AboutGridWrapperTypes = {
+    curs: string
+    moduls: string
+    empresa: string
+    hores: string
+    dates: string
+}
+
+
+export const AboutGridWrapper = ({curs, moduls, empresa, hores, dates}: AboutGridWrapperTypes) => {
+    return (
+        <StyledAboutGridWrapper>
+            <Curs>{curs}</Curs>
+            <Moduls>{moduls}</Moduls>
+            <Empresa>{empresa}</Empresa>
+            <Hores>{hores}</Hores>
+            <Dates>{dates}</Dates>
+        </StyledAboutGridWrapper>
+    );
+};
+
+const StyledAboutGridWrapper = styled.div`
+    display: grid;
+    grid-template-columns: repeat(9, 1fr);
+    gap: 20px;
+    
+    & > :first-child {
+        grid-area: 1/1/2/3;
+    }
+
+    & > :nth-child(2) {
+        grid-area: 1/3/2/6;
+    }
+
+    & > :nth-child(3) {
+        grid-area: 1/6/2/8;
+    }
+
+    & > :nth-child(4) {
+        grid-area: 1/8/2/9;
+    }
+
+    & > :last-child {
+        grid-area: 1/9/2/10;
+    }
+`
+
+const Curs = styled.p`
+
+`
+
+const Moduls = styled.p`
+    white-space: pre-line;
+`
+
+const Empresa = styled.p`
+
+`
+
+const Hores = styled.p`
+
+`
+
+const Dates = styled.p`
+
+`
