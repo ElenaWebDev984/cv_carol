b8d4564bc26ce1afb1b634327c60828b36e13d11
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-04 22:20:27 +0200

    created experienceData

---
+++ src/layout/sections/about/data/experienceData.ts
@@ -0,0 +1,482 @@
+export type ExperienceTypes = {
+    id: number;
+    curs: string;
+    moduls: string;
+    empresa: string;
+    hores: string;
+    dates: string;
+}
+
+export const experienceData: ExperienceTypes[] = [
+    {
+        id: 1,
+        curs: "Actualment impartint\n" +
+            "(SSCE0110) DOCÈNCIA DE LA FORMACIÓ PROFESSIONAL PER A L’OCUPACIÓ",
+        moduls: `Certificat de professionalitat del SOC – FOAP:
+                - MF1442_3 Programació didàctica d'accions formatives per a l'ocupació
+                - MF1444_3 Impartició i tutorització d'accions formatives per a l'ocupació
+                - MF1446_3 Orientació laboral i promoció de la qualitat en la formació professional per a l&#39;ocupació`,
+        empresa: "Centre de Formació Aptos – Inserció i servei",
+        hores: "190 hores",
+        dates: "Des d’abril fins a juny de 2025",
+    },
+    {
+        id: 2,
+        curs: "Actualment impartint\n" +
+            "ADGG0508 – OPERACIONS D’ENREGISTRAMENT I TRACTAMENT DE DADES I DOCUMENTS",
+        moduls: `Certificat de professionalitat del SOC.`,
+        empresa: "Ajuntament de Cambrils - Ateneu",
+        hores: "440 hores",
+        dates: "Des de febrer fins a juliol de 2025",
+    },
+    {
+        id: 3,
+        curs: "Actualment impartint\n" +
+            "COML0110 - Activitats auxiliars de magatzem",
+        moduls: `Certificat de professionalitat del SOC – FOAP
+            - MF1325_1: Operacions auxiliars d’emmagatzematge: 80 hores
+            - MF1326_1: Preparació de comandes: 40 hores`,
+        empresa: "Ajuntament de La Selva del Camp",
+        hores: "120 hores",
+        dates: "Març, abril i maig de 2025",
+    },
+    {
+        id: 4,
+        curs: "ADGD0308 – Activitats de gestió administrativa",
+        moduls: `MF0233 - Ofimàtica`,
+        empresa: "Centre de formació “Microbits Institut”(Reus)",
+        hores: "190 hores",
+        dates: "Gener i febrer de 2025",
+    },
+    {
+        id: 5,
+        curs: "COMT0211 – Activitats auxiliars de comerç",
+        moduls: `Certificat de professionalitat del SOC`,
+        empresa: "Centre de Formació Novatècnica (Tarragona)",
+        hores: "270 hores",
+        dates: "Gener i febrer de 2025",
+    },
+    {
+        id: 6,
+        curs: "COMT0211 – Activitats auxiliars de comerç",
+        moduls: `Certificat de professionalitat del SOC`,
+        empresa: "Centre de Formació Novatècnica (Tarragona)",
+        hores: "270 hores",
+        dates: "Setembre fins novembre de 2024",
+    },
+    {
+        id: 7,
+        curs: "ADGG0508 – OPERACIONS D’ENREGISTRAMENT I TRACTAMENT DE DADES I DOCUMENTS",
+        moduls: `Certificat de professionalitat del SOC.`,
+        empresa: "Centre de formació “Microbits Institut”(Reus)",
+        hores: "440 hores",
+        dates: "Des d’abril fins a agost de 2024",
+    },
+    {
+        id: 8,
+        curs: "INTRODUCCIÓ A LA INFORMÀTICA – PROGRAMES WORD I EXCEL",
+        moduls: `Programa mesures específiques d’accés al treball 
+            – Projecte primers contractes 
+            – Intermediació laboral`,
+        empresa: "Creu Roja (Reus)",
+        hores: "10 hores",
+        dates: "Juny 2024",
+    },
+    {
+        id: 9,
+        curs: "GESTIÓ DE MAGATZEM",
+        moduls: `Programa mesures específiques d’accés al treball 
+        – Projecte primers contractes – Intermediació
+laboral`,
+        empresa: "Creu Roja (Reus)",
+        hores: "55 hores",
+        dates: "Maig 2024",
+    },
+    {
+        id: 10,
+        curs: "Curs d’Atenció al client i Televenda",
+        moduls: `Programa barris desafavorits`,
+        empresa: "Calafell Ocupació",
+        hores: "25 hores",
+        dates: "Abril 2024",
+    },
+    {
+        id: 11,
+        curs: "COML0110 - Activitats auxiliars de magatzem",
+        moduls: `Certificat de professionalitat del SOC – FOAP
+            - MF1325_1: Operacions auxiliars d’emmagatzematge: 80 hores
+            - MF1326_1: Preparació de comandes: 40 hores`,
+        empresa: "Ajuntament de La Selva del Camp",
+        hores: "120 hores",
+        dates: "Febrer, març i abril 2024",
+    },
+    {
+        id: 12,
+        curs: "Formació a empreses Camping Resort Sangulí",
+        moduls: `3 jornades de formació:
+            - Protocol en el servei de restauració
+            - Gestió eficient de supermercats
+            - Tècniques de venda`,
+        empresa: "Contractada per IMFE Mas Carandell\n" +
+            "Lloc d’impartició de les jornades: Camping Resort Sangulí.",
+        hores: "15 hores",
+        dates: "5, 7 i 8 de març",
+    },
+    {
+        id: 13,
+        curs: "AADGG0508 – OPERACIONS D’ENREGISTRAMENT I TRACTAMENT DE DADES I DOCUMENTS",
+        moduls: `Certificat de professionalitat del SOC.`,
+        empresa: "Ajuntament de Cambrils - Ateneu",
+        hores: "440 hores",
+        dates: "Des de juny a novembre de 2023",
+    },
+    {
+        id: 14,
+        curs: "COML0110 - Activitats auxiliars de magatzem",
+        moduls: `CCertificat de professionalitat del SOC:
+            - MF1325_1: Operacions auxiliars d’emmagatzematge`,
+        empresa: "Centre de Formació Novatècnica – Vila-seca",
+        hores: "80 hores",
+        dates: "Juny i juliol 2023",
+    },
+    {
+        id: 15,
+        curs: "COML0110 - Activitats auxiliars de magatzem",
+        moduls: `Certificat de professionalitat del SOC:
+            - MF1325_1: Operacions auxiliars d’emmagatzematge`,
+        empresa: "Centre de Formació Novatècnica – Torreforta",
+        hores: "40 hores",
+        dates: "Juny 2023",
+    },
+    {
+        id: 16,
+        curs: "COML0110 - Activitats auxiliars de magatzem",
+        moduls: `Certificat de professionalitat del SOC – Projecte Singulars
+            - MF1325_1: Operacions auxiliars d’emmagatzematge: 80 hores
+            - MF1326_1: Preparació de comandes: 40 hores`,
+        empresa: "Centre de Formació Novatècnica – Vila-seca",
+        hores: "120 hores",
+        dates: "Març i abril de 2023",
+    },
+    {
+        id: 17,
+        curs: "ADGG0408 – Operacions auxiliars de serveis administratius i generals",
+        moduls: `Certificat de professionalitat del SOC`,
+        empresa: "Centre de Formació Aptos – Inserció i servei",
+        hores: "390 hores",
+        dates: "Febrer al juny de 2023",
+    },
+    {
+        id: 18,
+        curs: "COML0110 - Activitats auxiliars de magatzem",
+        moduls: `Certificat de professionalitat del SOC - FOAP
+            - MF1326_1: Preparació de comandes: 40 hores`,
+        empresa: "Vallsgenera – Centre de formació de l’Ajuntament de Valls",
+        hores: "40 hores",
+        dates: "Març 2023",
+    },
+    {
+        id: 19,
+        curs: "COML0110 - Activitats auxiliars de magatzem",
+        moduls: `Certificat de professionalitat del SOC – FOAP
+            - MF1325_1: Operacions auxiliars d’emmagatzematge: 80 hores
+            - MF1326_1: Preparació de comandes: 40 hores`,
+        empresa: "Ajuntament de La Selva del Camp",
+        hores: "120 hores",
+        dates: "Gener i febrer 2023",
+    },
+    {
+        id: 20,
+        curs: "COMT0211 – Activitats auxiliars de comerç",
+        moduls: `Certificat de professionalitat del SOC`,
+        empresa: "Centre de Formació Ocupacional de l’Ajuntament de Tortosa",
+        hores: "270 hores",
+        dates: "Setembre fins desembre de 2022",
+    },
+    {
+        id: 21,
+        curs: "ADGG0408 – Operacions auxiliars de serveis administratius i generals",
+        moduls: `Certificat de professionalitat del SOC`,
+        empresa: "Centre de Formació Ocupacional de l’Ajuntament de Tortosa",
+        hores: "390 hores",
+        dates: "Juny fins desembre de 2022",
+    },
+    {
+        id: 22,
+        curs: "COML0110 - Activitats auxiliars de magatzem",
+        moduls: `Certificat de professionalitat del SOC – Projecte Singulars
+            - MF1325_1: Operacions auxiliars d’emmagatzematge: 80 hores
+            - MF1326_1: Preparació de comandes: 40 hores`,
+        empresa: "Ajuntament de La Selva del Camp",
+        hores: "120 hores",
+        dates: "Maig i juny 2022",
+    },
+    {
+        id: 23,
+        curs: "COMT0211 – Activitats auxiliars de comerç",
+        moduls: `Certificat de professionalitat del SOC`,
+        empresa: "Centre de Formació Novatècnica - Tarragona",
+        hores: "270 hores",
+        dates: "Abril, maig i juny 2022",
+    },
+    {
+        id: 24,
+        curs: "Gestió de magatzem i logística",
+        moduls: `Projecte Singulars – Curs per a Nouvinguts
+            - COML008PO Gestió Bàsica de magatzem. Introducció. (20 hores)
+            - COML019PO - Organització de magatzem (30 hores)
+            - C0ML002PO - Traçabilitat i codificació (50 hores)`,
+        empresa: "Fundació EMI Manresa\n" +
+            "Lloc d’impartició: Casa del Mar (Tarragona)",
+        hores: "100 hores",
+        dates: "Des de març fins al maig de 2022",
+    },
+    {
+        id: 25,
+        curs: "COML0110 - Activitats auxiliars de magatzem",
+        moduls: `Certificat de professionalitat del SOC 
+            – Projecte Singulars
+            - MF1326_1: Preparació de comandes: 40 hores`,
+        empresa: "Ajuntament de La Selva del Camp",
+        hores: "40 hores",
+        dates: "Febrer i març de 2022",
+    },
+    {
+        id: 26,
+        curs: "COML0110 - Activitats auxiliars de magatzem",
+        moduls: `Certificat de professionalitat del SOC – Projecte Singulars
+            - MF1325_1: Operacions auxiliars d’emmagatzematge: 80 hores
+            - MF1326_1: Preparació de comandes: 40 hores`,
+        empresa: "Centre de Formació Novatècnica - Tarragona",
+        hores: "120 hores",
+        dates: "Febrer i març de 2022",
+    },
+    {
+        id: 27,
+        curs: "COML0110 - Activitats auxiliars de magatzem",
+        moduls: `Certificat de professionalitat del SOC – Projecte Singulars
+            - MF1325_1: Operacions auxiliars d’emmagatzematge: 80 hores
+            - MF1326_1: Preparació de comandes: 40 hores`,
+        empresa: "Centre de Formació Novatècnica – Vila-seca",
+        hores: "120 hores",
+        dates: "De gener al març de 2022",
+    },
+    {
+        id: 28,
+        curs: "COMT0211 – Activitats auxiliars de comerç",
+        moduls: `Certificat de professionalitat del SOC`,
+        empresa: "Centre de Formació Ocupacional de l’Ajuntament de Tortosa",
+        hores: "270 hores",
+        dates: "Abril fins juliol de 2021",
+    },
+    {
+        id: 29,
+        curs: '“Comerç i Logística”',
+        moduls: ``,
+        empresa: "Adreçat a joves al Centre de Noves Oportunitats de Tarragona.",
+        hores: "40 hores",
+        dates: "Juny - Juliol 2021",
+    },
+    {
+        id: 30,
+        curs: "HOTT01 - Accions bàsiques davant la covid-19 en turisme i hostaleria",
+        moduls: `Mòduls 3, 4, 5, 6 
+            – Modalitat: VIRTUAL`,
+        empresa: "Microdeltasoft (Amposta)",
+        hores: "60 hores",
+        dates: "Abril 2021",
+    },
+    {
+        id: 31,
+        curs: '“Inserció laboral, sensibilització mediambiental i en la Igualtat de gènere”',
+        moduls: ``,
+        empresa: "Curs de socorrisme en instal·lacions aquàtiques " +
+            "- Creu Roja de Tarragona",
+        hores: "10 hores",
+        dates: "Abril 2021",
+    },
+    {
+        id: 32,
+        curs: '“Inserció laboral, sensibilització mediambiental i en la Igualtat de gènere”',
+        moduls: ``,
+        empresa: "Curs de socorrisme en espais aquàtics naturals " +
+            "– Creu Roja de Tarragona",
+        hores: "10 hores",
+        dates: "Març 2021",
+    },
+    {
+        id: 33,
+        curs: `“Acció d’aprenentatge especialitzat polivalent en l’àmbit de magatzem”`,
+        moduls: ``,
+        empresa: "Curs del SOC per a persones en atur –\n" +
+            "Consell Comarcal de la Terra Alta (Gandesa)",
+        hores: "90 hores",
+        dates: "Octubre i novembre de 2020",
+    },
+    {
+        id: 34,
+        curs: "ADG0408 – Operacions Auxiliars de serveis administratius i generals (nivell 1)",
+        moduls: `MF0969 Tècniques administratives bàsiques d’oficina.`,
+        empresa: "Mas Carandell (Reus)\n" +
+            "Modalitat: Presencial i VIRTUAL",
+        hores: "150 hores",
+        dates: "Gener a agost de 2020",
+    },
+    {
+        id: 35,
+        curs: "ADG0408 – Operacions Auxiliars de serveis administratius i generals (nivell 1)",
+        moduls: `UF520 Comunicació en les Relacions Professionals`,
+        empresa: "Centre de formació BRM (Móra d’Ebre)",
+        hores: "48 hores",
+        dates: "Gener - març 2020",
+    },
+    {
+        id: 36,
+        curs: '(HOTA0308) "Recepció en allotjaments"',
+        moduls: `Mòdul MF0263 Accions comercials i reserves`,
+        empresa: "Associació hotelera Salou, Cambrils, La Pineda.",
+        hores: "150 hores",
+        dates: "Novembre de 2019",
+    },
+    {
+        id: 37,
+        curs: '“Seguretat i prevenció en les operacions auxiliars d’emmagatzematge. Manipulació de càrregues”',
+        moduls: ``,
+        empresa: "Empresa Dex Ibérica (Vila – seca) per atreballadors de l’empresa.",
+        hores: "8 hores",
+        dates: "Novembre 2019",
+    },
+    {
+        id: 38,
+        curs: '”Auxiliar de Magatzem”',
+        moduls: `Mòdul “Introducció al magatzem”`,
+        empresa: "Curs per a Refugiats al Centre Mas Carandell (Reus)",
+        hores: "9 hores",
+        dates: "Octubre 2019",
+    },
+    {
+        id: 39,
+        curs: '”Auxiliar de Magatzem”',
+        moduls: `Mòdul “Preparació de comandes”`,
+        empresa: "Curs per a joves. Centre Mas Carandell (Reus) " +
+            "– Entitat organitzadora: CNO Tarragona",
+        hores: "40 hores",
+        dates: "Setembre 2019",
+    },
+    {
+        id: 40,
+        curs: "ADGG0208 – Activitats administratives en la relació amb el client (nivell 2)",
+        moduls: `- MF 0233 Ofimàtica (190 h)
+            - MF0973 Enregistrament de dades (90 h.)
+            - MF0978 Gestió d’arxius (60 h.)`,
+        empresa: "Centre de formació BRM (Móra d’Ebre)",
+        hores: "",
+        dates: "Febrer - Juny 2019",
+    },
+    {
+        id: 41,
+        curs: "ADG0408 – Operacions Auxiliars de serveis administratius i generals (nivell 1)",
+        moduls: `UF520 Comunicació en les Relacions Professionals`,
+        empresa: "Centre de formació BRM (Móra d’Ebre)",
+        hores: "48 hores",
+        dates: "Febrer - Juny 2019",
+    },
+    {
+        id: 42,
+        curs: "ADG0408 – Operacions Auxiliars de serveis administratius i generals (nivell 1)",
+        moduls: `MF0969 Tècniques administratives bàsiques d’oficina`,
+        empresa: "Mas Carandell (Reus)",
+        hores: "150 hores",
+        dates: "Febrer a Juliol de 2019",
+    },
+    {
+        id: 43,
+        curs: '“Logística i Atenció al client”',
+        moduls: ``,
+        empresa: "Curs per a joves a El Vendrell " +
+            "– Fundació Intermedia – Formació i treball UTE – CNO Tarragona",
+        hores: "90 hores",
+        dates: "2018 - 2019",
+    },
+    {
+        id: 44,
+        curs: '”Auxiliar de Magatzem”',
+        moduls: ``,
+        empresa: "Curs per a joves. Centre Mas Carandell\n" +
+            "Entitat organitzadora: CNO Tarragona",
+        hores: "70 hores",
+        dates: "Octubre 2018",
+    },
+    {
+        id: 45,
+        curs: `“Dinamització, Programació i desenvolupament d’accions culturals” (SSCB0110)`,
+        moduls: ``,
+        empresa: "CCEPTA Tarragona",
+        hores: "420 hores",
+        dates: "2018",
+    },
+    {
+        id: 46,
+        curs: '“Gestió de Protocol”',
+        moduls: `Mòdul de “Protocol a l’Empresa”.`,
+        empresa: "CEPTA Tarragona",
+        hores: "16 hores",
+        dates: "2017",
+    },
+    {
+        id: 47,
+        curs: '“Dinamització Comunitària” (SSC321_3)',
+        moduls: ``,
+        empresa: "CEPTA Tarragona",
+        hores: "110 hores",
+        dates: "2017",
+    },
+    {
+        id: 48,
+        curs: '“Recepció en allotjaments” (HOTA0308)',
+        moduls: ``,
+        empresa: 'Mòdul “Recepció i atenció al client”',
+        hores: "150 hores",
+        dates: "2017",
+    },
+    {
+        id: 49,
+        curs: `“Dinamització, Programació i desenvolupament d’accions culturals” (SSCB0110)`,
+        moduls: ``,
+        empresa: "CEPTA Tarragona",
+        hores: "420 hores",
+        dates: "2017",
+    },
+    {
+        id: 50,
+        curs: '“Promoció turística local i informació al visitant” (HOTI0108)',
+        moduls: ``,
+        empresa: "“Promoció turística local i informació al visitant” (HOTI0108)",
+        hores: "690 hores",
+        dates: "2017",
+    },
+    {
+        id: 51,
+        curs: `“Dinamització, Programació i desenvolupament d’accions culturals” (SSCB0110)`,
+        moduls: ``,
+        empresa: "CEPTA Tarragona",
+        hores: "420 hores",
+        dates: "2016",
+    },
+    {
+        id: 52,
+        curs: '“Gestió, Protocol i Comunicació Administrativa”',
+        moduls: `“Protocol a l’Empresa”.`,
+        empresa: "CEPTA Tarragona",
+        hores: "5 hores",
+        dates: "2016",
+    },
+    {
+        id: 53,
+        curs: '“Recepció en allotjaments” (HOTA0308)',
+        moduls: ``,
+        empresa: "Cambra de Comerç de Tarragona",
+        hores: "630 hores",
+        dates: "2016",
+    },
+];
