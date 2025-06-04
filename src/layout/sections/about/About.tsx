d2db1634a7137a2fc91768528f814dd267d4e60f
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-04 22:38:24 +0200

    changed language by catalan

--- src/layout/sections/about/About.tsx
+++ src/layout/sections/about/About.tsx
@@ -6,16 +6,17 @@ export const About = () => {
     return (
         <StyledAbout>
             <AboutContent>
-                <AboutTitle>About Me</AboutTitle>
-                <AboutText>The Generator App is an online tool that helps you to export ready-made templates ready to work as your future website. It helps you to combine slides, panels and other components and export it as a set of static files: HTML/CSS/JS.</AboutText>
-                <AboutTitle>Work Experience</AboutTitle>
-                <AboutGridWrapper>
-
-                </AboutGridWrapper>
-                <AboutTitle>Education</AboutTitle>
-                <AboutGridWrapper>
-
-                </AboutGridWrapper>
+                <AboutTitle>Sobre mi</AboutTitle>
+                <AboutText>Des del 2016 i fins l’actualitat, imparteixo cursos de formació ocupacional adreçats a persones en atur de totes les edats i cursos a empreses, per a treballadors en actiu:</AboutText>
+                <AboutTitle>Experiència Laboral</AboutTitle>
+                <AboutGridWrapper curs='Actualment impartint (SSCE0110) DOCÈNCIA DE LA FORMACIÓ PROFESSIONAL PER A L’OCUPACIÓ' moduls='Certificat de professionalitat del SOC – FOAP:
+                - MF1442_3 Programació didàctica d&#39; accions formativesper a l&#39;ocupació
+                - MF1444_3 Impartició i tutorització d&#39;accions formatives per a l&#39;ocupació
+                - MF1446_3 Orientació laboral i promoció de la qualitat en la formació professional per a l&#39;ocupació' empresa='Centre de Formació Aptos – Inserció i servei' hores='190 hores' dates='Des d’abril fins a juny de 2025'/>
+                <AboutTitle>Formació Acadèmica</AboutTitle>
+                <AboutGridWrapper curs={""} moduls={""} empresa={""} hores={""} dates={""}/>
+                <AboutTitle>Formació Compementària</AboutTitle>
+                <AboutGridWrapper curs={""} moduls={""} empresa={""} hores={""} dates={""}/>
             </AboutContent>
         </StyledAbout>
     );
