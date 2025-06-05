99616b1c5cbe13c8c12d0b7ec5da8bfedf77b7ee
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-05 22:06:51 +0200

    created PerfilComponent

--- src/layout/sections/about/Perfil.tsx
+++ src/layout/sections/about/Perfil.tsx
@@ -8,17 +8,17 @@ export const Perfil = () => {
        <FlexWrapper>
            <Photo src={photo2} alt="photo"/>
            <ul>
-               <li></li>
-               <li></li>
-               <li></li>
-               <li></li>
-               <li></li>
-               <li></li>
-               <li></li>
-               <li></li>
-               <li></li>
-               <li></li>
-               <li></li>
+               <li>Actitud responsable i resolutiva.</li>
+               <li>Habilitats interpersonals.</li>
+               <li>Interès pel desenvolupament professional.</li>
+               <li>Flexibilitat</li>
+               <li>Facilitat d’adaptació al canvi.</li>
+               <li>Persona proactiva i dinàmica.</li>
+               <li>Persona oberta i optimista amb facilitat per a contagiar l’entusiasme.</li>
+               <li>Persona formal i compromesa amb el lloc de feina i l’empresa.</li>
+               <li>Nivell avançat d’office.</li>
+               <li>Disseny de cursos.</li>
+               <li>Experiència a les aules.</li>
            </ul>
        </FlexWrapper>
     );
