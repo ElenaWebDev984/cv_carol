be79565db77b711fd5333570b66049552e056d23
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-06 23:02:52 +0200

    added FlexWrapper

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,13 +1,33 @@
 import styled from "styled-components";
 import {theme} from "../../styles/Theme.ts";
 import {Container} from "../../components/Container.tsx";
+import {MenuSocial} from "../header/MenuSocial.tsx";
+import {Icon} from "../../components/icon/Icon.tsx";
+import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 
 
 export const Footer = () => {
     return (
         <StyledFooter>
             <Container>
-                <Copyright>© 2025 Dissenyat i construït per <span>Elena Badalova</span> amb <span>Amor</span> & <span>Cafè</span></Copyright>
+                <FlexWrapper direction="column" align='center'>
+                    <Location>
+                        <li>
+                            <Icon iconId='phone'/>
+                            619847748
+                        </li>
+                        <li>
+                            <Icon iconId='email'/>
+                            carol.tarrio.profe@hotmail.com
+                        </li>
+                        <li>
+                            <Icon iconId='location'/>
+                            43850 – Cambrils
+                        </li>
+                    </Location>
+                    <MenuSocial/>
+                    <Copyright>© 2025 Dissenyat i construït per <span>Elena Badalova</span> amb <span>Amor</span> & <span>Cafè</span></Copyright>
+                </FlexWrapper>
             </Container>
         </StyledFooter>
     );
@@ -17,8 +37,21 @@ const StyledFooter = styled.footer`
     background-color: ${theme.colors.accentColor};
 `
 
+const Location = styled.ul`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    gap: 50px;
+    
+    li {
+        display: flex;
+        text-align: center;
+    }
+`
+
 const Copyright = styled.p`
-   //font-weight: bold;
+    //font-weight: bold;
     line-height: 1.4;
     color: ${theme.colors.testimonyTextFont};
 
