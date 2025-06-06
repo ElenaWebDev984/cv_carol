a668e800b16f9a63b236df32e2e2199dd9d565ec
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-06 21:49:20 +0200

    added gradientColor

--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -5,6 +5,7 @@ export const theme = {
         accentColor: '#FB3F5C',
         primaryFont: '#191018',
         headerFooterIconsFont: '#F8EEEF',
+        linearGradientFont: 'linear-gradient(90deg, #13B0F5 2.6%, #E70FAA 100%)',
         workFont: '#2D2D2D',
         holaFont: '#000',
         testimonyNameFont: '#05060A',
