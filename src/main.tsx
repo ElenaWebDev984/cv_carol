51f63bdaf4593b6638b5dd4db5124b9305e2111c
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-06 20:37:01 +0200

    added GloblStyled

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,12 @@
-
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import './index.css'
 import {App} from './App.tsx'
+import {GlobalStyle} from "./styles/Global.styled.ts";
+import {StrictMode} from "react";
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+        <GlobalStyle/>
+        <App/>
+    </StrictMode>
+)
--- src/styles/Global.styled.ts
+++ src/styles/Global.styled.ts
@@ -1,14 +1,32 @@
 import {createGlobalStyle} from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
-    body {
+    *,
+    *::before,
+    *::after {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
-        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    }
+
+    body {
+        font-family: 'Nunito Sans', -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
         'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
         sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
+    }
+    
+    a {
+        text-decoration: none;
+    }
+    
+    ul {
+        list-style: none;
+    }
+    
+    button {
+        border: none;
+        background-color: unset;
     }
 `
