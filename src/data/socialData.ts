533c34ee1bcfe5c8e3f7626ef4e36ccd43c06b4e
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-06 23:02:25 +0200

    updated socialData

---
+++ src/data/socialData.ts
@@ -0,0 +1,32 @@
+export const socialData = [
+    {
+        id: 1,
+        href: 'https://x.com/',
+        iconId: 'twitter',
+        target: '_blank',
+    },
+    {
+        id: 2,
+        href: 'https://www.instagram.com/',
+        iconId: 'instagram',
+        target: '_blank',
+    },
+    {
+        id: 3,
+        href: 'https://youtube.com/',
+        iconId: 'youtube',
+        target: '_blank',
+    },
+    {
+        id: 4,
+        href: 'https://www.facebook.com/',
+        iconId: 'facebook',
+        target: '_blank',
+    },
+    {
+        id: 5,
+        href: 'https://www.snapchat.com/',
+        iconId: 'snapchat',
+        target: '_blank',
+    },
+]
