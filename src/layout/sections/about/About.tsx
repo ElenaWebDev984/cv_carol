33bf0fe847f2b9f30c6ee17f686d90d2e415b6da
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-06 21:26:17 +0200

    added Container

--- src/layout/sections/about/About.tsx
+++ src/layout/sections/about/About.tsx
@@ -3,20 +3,23 @@ import {AboutExperience} from "./AboutExperience.tsx";
 import {AboutEducation} from "./AboutEducation.tsx";
 import {AboutEducationComplementaria} from "./AboutEducationComplementaria.tsx";
 import {Perfil} from "./Perfil.tsx";
+import {Container} from "../../../components/Container.tsx";
 
 
 export const About = () => {
     return (
         <StyledAbout>
-            <AboutContent>
-                <AboutTitle>Sobre mi</AboutTitle>
-                <Perfil/>
-                <AboutText>Des del 2016 i fins l’actualitat, imparteixo cursos de formació ocupacional adreçats a persones en atur de totes les edats i cursos a empreses, per a treballadors en actiu:</AboutText>
+            <Container>
+                <AboutContent>
+                    <AboutTitle>Sobre mi</AboutTitle>
+                    <Perfil/>
+                    <AboutText>Des del 2016 i fins l’actualitat, imparteixo cursos de formació ocupacional adreçats a persones en atur de totes les edats i cursos a empreses, per a treballadors en actiu:</AboutText>
 
-                <AboutExperience/>
-                <AboutEducation/>
-                <AboutEducationComplementaria/>
-            </AboutContent>
+                    <AboutExperience/>
+                    <AboutEducation/>
+                    <AboutEducationComplementaria/>
+                </AboutContent>
+            </Container>
         </StyledAbout>
     );
 };
