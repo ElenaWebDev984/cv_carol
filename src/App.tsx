a58a5cd75a200c5f2f3882db260ae47175b6ecf3
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-06 21:27:41 +0200

    added Footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import {Header} from "./layout/header/Header.tsx";
 import {Main} from "./layout/sections/main/Main.tsx";
 import {About} from "./layout/sections/about/About.tsx";
+import {Footer} from "./layout/footer/Footer.tsx";
 
 
 export const App = () => {
@@ -10,6 +11,7 @@ export const App = () => {
             <Header/>
             <Main/>
             <About/>
+            <Footer/>
         </div>
     )
 }
