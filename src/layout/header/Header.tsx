47d8b4015a9cb1a642ac40a1780ab6fc1e800f0a
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-06 21:20:17 +0200

    added Container

--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,20 +1,25 @@
 import styled from "styled-components";
 import {Logo} from "../../components/logo/Logo.tsx";
 import {Menu} from "../../components/menu/Menu.tsx";
+import {Container} from "../../components/Container.tsx";
+import {theme} from "../../styles/Theme.ts";
+import {FlexWrapper} from "../../components/FlexWrapper.tsx";
 
 
 export const Header = () => {
     return (
         <StyledHeader>
-            <Logo/>
-            <Menu/>
+           <Container>
+               <FlexWrapper justify="space-between" align='center'>
+                   <Logo/>
+                   <Menu/>
+               </FlexWrapper>
+           </Container>
         </StyledHeader>
     );
 };
 
 const StyledHeader = styled.header`
-    background-color: #FB3F5C;
-    display: flex;
-    justify-content: space-between;
+    background-color: ${theme.colors.accentColor};
 `
 
