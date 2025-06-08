8d713d7399d9357edb2b8e7c10fd95865ee7d1c2
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-09 00:12:09 +0200

    deleted title

--- src/layout/sections/experiencia/ExperienciaLaboral.tsx
+++ src/layout/sections/experiencia/ExperienciaLaboral.tsx
@@ -1,13 +1,11 @@
 import {ExperienceGridWrapper} from "./ExperienceGridWrapper.tsx";
 import {experienceData} from "../../../data/about/experienceData.ts";
-import {AboutTitle} from "./About.tsx";
 
 
 
-export const AboutExperience = () => {
+export const ExperienciaLaboral = () => {
     return (
         <>
-            <AboutTitle>Experiència Laboral</AboutTitle>
             {experienceData.map((course) => (
                 <ExperienceGridWrapper key={course.id} {...course} />
             ))}
