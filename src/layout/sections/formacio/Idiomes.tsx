422f0a5109103e843a7d19a68774c7a37ae8de2e
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-10 16:44:25 +0200

    added FlexWrapper and Skills

--- src/layout/sections/formacio/Idiomes.tsx
+++ src/layout/sections/formacio/Idiomes.tsx
@@ -9,8 +9,11 @@ export const Idiomes = () => {
     return (
         <StyledIdiomesSkills>
             <Subtitle title='Idiomes'/>
-            <FlexWrapper>
-                <Skill/>
+            <FlexWrapper wrap='wrap' gap='30px'>
+                <Skill iconID='catalan' title='Títol de Català' subtitle='Nivell C' text='Final Batxillerat any 1994'/>
+                <Skill iconID='france' title='Títol acadèmic de Llengua Francesa' subtitle='Escola Oficial
+                d’Idiomes (EOI) de Tarragona' text='1995'/>
+                <Skill iconID='english' title='Anglès' subtitle='Certificat B1' text='Centre UNED – Juny 2024'/>
             </FlexWrapper>
 
         </StyledIdiomesSkills>
@@ -18,7 +21,5 @@ export const Idiomes = () => {
 };
 
 const StyledIdiomesSkills = styled.div`
-    h3 {
-        text-align: center;
-    }
+    
 `
