b23ecd9ffeeed8ca095b4a6ee8a447778f99899c
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-10 08:15:48 +0200

    added FlexWrapper

--- src/layout/sections/formacio/Formacio.tsx
+++ src/layout/sections/formacio/Formacio.tsx
@@ -5,17 +5,20 @@ import {FormacioComplementaria} from "./FormacioComplementaria.tsx";
 import {Title} from "../../../components/Title.tsx";
 import {Idiomes} from "./Idiomes.tsx";
 import {Informatica} from "./Informatica.tsx";
+import {FlexWrapper} from "../../../components/FlexWrapper.tsx";
 
 
 export const Formacio = () => {
     return (
         <StyledFormacio>
             <Container>
-                <Title title='Formació' />
-                <FormacioUni/>
-                <FormacioComplementaria/>
-                <Idiomes/>
-                <Informatica/>
+               <FlexWrapper direction='column'>
+                   <Title title='Formació' />
+                   <FormacioUni/>
+                   <FormacioComplementaria/>
+                   <Idiomes/>
+                   <Informatica/>
+               </FlexWrapper>
             </Container>
         </StyledFormacio>
     );
