20da761db3fa37cc0a3826ce4a81e165f8e674b4
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-11 21:47:14 +0200

    added Mask for Links

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import {theme} from "../../styles/Theme.ts";
 
 
 
@@ -8,9 +9,16 @@ export const Menu = (props: {menuItems: Array<string>}) => {
             <ul>
                 {props.menuItems.map((item, index) => {
                     return (
-                        <li key={index}>
-                            <a href="#">{item}</a>
-                        </li>
+                        <ListItem key={index}>
+                            <Link href="#">{item}
+                                <Mask>
+                                    <span>{item}</span>
+                                </Mask>
+                                <Mask>
+                                    <span>{item}</span>
+                                </Mask>
+                            </Link>
+                        </ListItem>
                     )
                 })}
             </ul>
@@ -25,3 +33,45 @@ const StyledMenu = styled.nav`
     }
 `
 
+const Mask = styled.tspan`
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: inline-block;
+    height: 50%;
+    overflow-y: hidden;
+    //outline: 1px solid darkviolet;
+    color: ${theme.colors.testimonyTextFont};
+//    secondaryBgc
+    
+    & + & {
+        top: 50%;
+        
+        span {
+            display: inline-block;
+            transform: translateY(-50%);
+        }
+    }
+`
+
+const ListItem = styled.li`
+    position: relative;
+    
+    &:hover {
+        ${Mask} {
+            transform: skewX(12deg)  translateX(5px);
+            
+            & + ${Mask} {
+                transform: skewX(12deg)  translateX(-5px);
+            }
+        }
+        
+        
+    }
+`
+
+const Link = styled.a`
+    font-weight: 400;
+    font-size: 24px;
+    color: ${theme.colors.headerFooterIconsFont};
+`
