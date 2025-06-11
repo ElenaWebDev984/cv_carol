ca0f2d36ac92306ac95cb3b46e19d148f710b9fb
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-11 23:17:00 +0200

    added btn and PhotoWrapper for Main

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -6,6 +6,7 @@ import {Container} from "../../../components/Container.tsx";
 import {theme} from "../../../styles/Theme.ts";
 
 
+
 export const Main = () => {
     return (
         <StyledMain>
@@ -15,9 +16,11 @@ export const Main = () => {
                         <Greeting>Hola a tothom!!!</Greeting>
                         <Name>Soc <span>Carol Tarrío Viñuales</span></Name>
                         <MainTitle>Formadora de cursos de formació ocupacional i de cursos per a empreses</MainTitle>
+                        <Button>Download CV</Button>
                     </div>
-
-                    <Image src={photo} alt="photo"/>
+                    <PhotoWrapper>
+                        <Image src={photo} alt="photo"/>
+                    </PhotoWrapper>
                 </FlexWrapper>
             </Container>
         </StyledMain>
@@ -52,6 +55,37 @@ const MainTitle = styled.h1`
     font-size: 30px;
     vertical-align: middle;
     //text-transform: lowercase;
+`
 
+const Button = styled.button`
+    background: #FB3F5C;
+    width: 220px;
+    height: 65px;
+    border-radius: 18px;
+    padding: 20px 60px;
+    margin-top: 20px;
+
+    color: #F8EEEF;
+    font-weight: 400;
+    font-size: 20px;
+    white-space: nowrap;
+    text-align: center;
+`
+
+const PhotoWrapper = styled.div`
+    position: relative;
+    z-index: 0;
+    
+    &::before {
+        content: '';
+        width: 620px;
+        height: 690px;
+        top: -65px;
+        left: -30px;
+        background: #FB3F5C80;
+        
+       position: absolute;
+        z-index: -1;
+    }
 `
 
