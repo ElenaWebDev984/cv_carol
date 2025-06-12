58c6b6b5c9e9a268155baac2db8339d245917f80
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-06-12 21:57:02 +0200

    created AficionsComponent

--- src/layout/sections/aficions/Aficions.tsx
+++ src/layout/sections/aficions/Aficions.tsx
@@ -1,1 +1,25 @@
-// Viatjar, lectura, cinema, cuinar,natació i caminar per la muntanya.
+import {SectionSubtitle} from "../../../components/SectionSubtitle.tsx";
+import {PerfilList} from "../sobreMi/SobreMe.tsx";
+import {Container} from "../../../components/Container.tsx";
+import styled from "styled-components";
+import {theme} from "../../../styles/Theme.ts";
+
+export const Aficions = () => {
+    return (
+        <StyledAficions>
+            <Container>
+                <PerfilList>
+                    <li>
+                        <SectionSubtitle title='Aficions'/>
+                    </li>
+                    <li>Viatjar, lectura, cinema, cuinar,natació i caminar per la muntanya.</li>
+                </PerfilList>
+            </Container>
+        </StyledAficions>
+    );
+};
+
+const StyledAficions = styled.section`
+    background-color: ${theme.colors.secondaryBgc};
+`
+
